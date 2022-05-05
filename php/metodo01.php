e648a8cab70b9b3660694538657bd2dd907ea550
ALoca19 <anitaynala17@gmail.com>
2022-05-04 19:40:40 -0500

    Completando el funcionamiento de muestra del metodo 01

--- php/metodo01.php
+++ php/metodo01.php
@@ -7,4 +7,15 @@ $Imagen = addslashes(file_get_contents($_FILES['image']['tmp_name']));
 
 $query = "INSERT INTO `tabla imagen` (`nombre`, `imagen`) VALUES ('$nombreImagen', '$Imagen')";
 
+$Resultado = $conexion -> query($query);
+
+if($Resultado)
+{
+    echo "Si se inserto";
+}
+else
+{
+    echo "no se inserto";
+}
+
 ?>
